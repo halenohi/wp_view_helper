73346f193c73919a9340a60354759fe1b5386526
kozo yamagata <tune002@gmail.com>
2014-02-06 15:47:02 +0900

    ページタイトルのバリエーションを増やしてメソッド名を変更

--- helpers/page_title_helper.php
+++ helpers/page_title_helper.php
@@ -5,23 +5,51 @@ trait PageTitleHelper {
     if (is_404()) {
       return 'お探しのページは見つかりませんでした 404 Not Found' . $divider;
     } elseif (is_category()) {
-      return $this->category_name() . $divider;
+      return $this->category_page_title() . $divider;
     } elseif (is_single()) {
-      return $this->single_name() . $divider;
+      return $this->single_page_title() . $divider;
     } elseif (is_page()) {
-      return $this->single_name() . $divider;
+      return $this->single_page_title() . $divider;
+    } elseif (is_tag()) {
+      return $this->tag_page_title() . $divider;
+    } elseif (is_author()) {
+      return $this->author_page_title() . $divider;
+    } elseif (is_archive()) {
+      return $this->archive_page_title() . $divider;
     }
   }
 
-  public function category_name() {
+  public function category_page_title() {
     global $wp_query;
     $category = $wp_query->queried_object;
     $parent_name = $category->category_parent > 0 ? ' ' . get_term($category->category_parent, 'category')->name : '';
     return $category->name . $parent_name;
   }
 
-  public function single_name() {
+  public function single_page_title() {
     global $wp_query;
     return $wp_query->post->post_title;
+  }
+
+  public function tag_page_title() {
+    global $wp_query;
+    return 'タグ：' . $wp_query->query['tag'];
+  }
+
+  public function author_page_title() {
+    global $wp_query;
+    return '執筆者：' . $wp_query->queried_object->data->display_name;
+  }
+
+  public function archive_page_title() {
+    global $wp_query;
+    $result = 'アーカイブ：';
+    if (isset($wp_query->query['year'])) {
+      $result .= $wp_query->query['year'] . '年';
+    }
+    if (isset($wp_query->query['monthnum'])) {
+      $result .= $wp_query->query['monthnum'] . '月';
+    }
+    return $result;
   }
 }
