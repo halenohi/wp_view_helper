c99265ae366df87a788aaeff780f22a7c0d089a9
kozo yamagata <tune002@gmail.com>
2014-09-04 16:08:09 +0900

    PartialHelper#partialでmustacheを使えるようにした

--- helpers/partial_helper.php
+++ helpers/partial_helper.php
@@ -1,8 +1,34 @@
 <?php
 trait PartialHelper {
-  public function partial($relative_path, $theme_name = '') {
+  private $template_engine;
+  private $template_engine_name = 'Mustache_Engine';
+  private $template_extension = 'mustache';
+
+  public function partial($relative_path, $theme_name = '', $local_assigns = null) {
+    $partial_path = $this->makePartialPath($relative_path, $theme_name);
+    if (!file_exists($partial_path)) {
+      return '';
+    }
+    return $this->render($partial_path, $local_assigns);
+  }
+
+  private function makePartialPath($relative_path, $theme_name) {
     $root_path = empty($theme_name) ? $this->theme_root_path : $this->change_theme_name($theme_name);
-    $partial_path = dirname(WP_CONTENT_DIR) . $root_path . $relative_path;
-    return file_get_contents($partial_path);
+    return dirname(WP_CONTENT_DIR) . $root_path . $relative_path;
+  }
+
+  private function template() {
+    if (empty($this->template_engine)) {
+      $this->template_engine = new $this->template_engine_name;
+    }
+    return $this->template_engine;
+  }
+
+  private function render($partial_path, $local_assigns) {
+    $partial_str = file_get_contents($partial_path);
+    if (preg_match('/\.' . $this->template_extension . '$/', $partial_path)) {
+      $partial_str = $this->template()->render($partial_str, $local_assigns);
+    }
+    return $partial_str;
   }
 }
