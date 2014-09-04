020bb6443a881729a938377f80bd12aca90fcc5d
kozo yamagata <tune002@gmail.com>
2014-09-04 13:21:38 +0900

    を変更できるようにした

--- helpers/asset_helper.php
+++ helpers/asset_helper.php
@@ -8,23 +8,32 @@ trait AssetHelper {
     $this->theme_root_path = $theme_path . '/';
   }
 
-  public function asset_path($file_name, $asset_type, $extension = '') {
-    $path = $this->theme_root_path . $asset_type . 's/' . $file_name;
+  private function change_theme_name($new_name) {
+    $split_path = explode('/', $this->theme_root_path);
+    array_pop($split_path);
+    array_pop($split_path);
+    $split_path[] = $new_name;
+    return implode('/', $split_path) . '/';
+  }
+
+  public function asset_path($file_name, $asset_type, $extension = '', $theme_name = '') {
+    $root_path = empty($theme_name) ? $this->theme_root_path : $this->change_theme_name($theme_name);
+    $path = $root_path . $asset_type . 's/' . $file_name;
     if ($asset_type !== 'image') {
       $path .= '.' . $extension;
     }
     return $path;
   }
 
-  public function stylesheet_path($file_name) {
-    return $this->asset_path($file_name, 'stylesheet', 'css');
+  public function stylesheet_path($file_name, $theme_name = '') {
+    return $this->asset_path($file_name, 'stylesheet', 'css', $theme_name);
   }
 
-  public function javascript_path($file_name) {
-    return $this->asset_path($file_name, 'javascript', 'js');
+  public function javascript_path($file_name, $theme_name = '') {
+    return $this->asset_path($file_name, 'javascript', 'js', $theme_name);
   }
 
-  public function image_path($file_name) {
-    return $this->asset_path($file_name, 'image');
+  public function image_path($file_name, $theme_name = '') {
+    return $this->asset_path($file_name, 'image', $theme_name);
   }
 }
